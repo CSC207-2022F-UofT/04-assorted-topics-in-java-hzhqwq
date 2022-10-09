11fb9d20e3110d9e6a86e30d1388f03f600ec231
ariahu <aria.hu@mail.utoronto.ca>
2022-10-09 02:05:19 -0400

    t1

--- src/main/java/DrivableTrader.java
+++ src/main/java/DrivableTrader.java
@@ -11,3 +11,19 @@
  */
 
 import java.util.List;
+public class DrivableTrader extends Trader<Drivable>{
+    public DrivableTrader(List<Drivable> inventory, List<Drivable> wishlist, int money){
+        super(inventory, wishlist, money);
+    }
+
+    public int getSellingPrice(Drivable item) {
+        int super_price = super.getSellingPrice(item);
+
+        if (super_price == Tradable.MISSING_PRICE) {
+            return super_price;
+        }
+
+        return super_price + item.getMaxSpeed();
+
+    }
+}
